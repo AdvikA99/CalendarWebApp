92d1f06302fb301d8061e770c5236d4685af4088
AdvikA99 <57047411+AdvikA99@users.noreply.github.com>
2024-01-21 17:30:48 +0800

    Added year item tooltip arrow and vertical screen clamping

--- src/components/Calendar/YearView/YearItemTooltip/YearItemTooltip.tsx
+++ src/components/Calendar/YearView/YearItemTooltip/YearItemTooltip.tsx
@@ -8,6 +8,8 @@ function YearItemTooltip(props : any) {
   const [leftPosition, setLeftPosition] = useState('50%');
   const [isTooltipVisible, setTooltipVisible] = useState(false);
 
+  const [showOnTop, setShowOnTop] = useState(true);
+
   useEffect(() => {
     const updatePosition = () => {
       if (tooltipParentRef.current && tooltipRef.current) {
@@ -15,8 +17,10 @@ function YearItemTooltip(props : any) {
         const rect = tooltipRef.current.getBoundingClientRect();
         
         const x1 = rectParent.x + rectParent.width/2 - rect.width/2;
-        const x2 = Math.max(24, x1) - rect.width/2 + rectParent.width/2;
+        const x2 = Math.max(16, x1) - rect.width/2 + rectParent.width/2;
         setLeftPosition(`calc(${x2 - x1}px)`);
+
+        setShowOnTop(rectParent.y - rect.height >= 8);
       }
     };
 
@@ -50,8 +54,15 @@ function YearItemTooltip(props : any) {
       <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
         {props.children}
       </div>
+      {isTooltipVisible && (   
+        <div id="yearItemTooltipArrow" className={showOnTop ? "yearItemTooltipArrowTop" : "yearItemTooltiArrowBottom"}></div>
+      )}
       {isTooltipVisible && (
-        <div id="yearItemTooltip" ref={tooltipRef} style={{left: leftPosition }}>
+        <div 
+          id="yearItemTooltip" 
+          ref={tooltipRef} 
+          style={{left: leftPosition}}
+          className={showOnTop ? "yearItemTooltipTop" : "yearItemTooltipBottom"}>
           <p>{month} {date.getDate() + dateSuffix}</p>
         </div>
       )}
