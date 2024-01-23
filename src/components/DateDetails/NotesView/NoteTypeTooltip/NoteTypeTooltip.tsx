09528df3f9b36d52271594f6f2bb5139b2135c83
AdvikA99 <57047411+AdvikA99@users.noreply.github.com>
2024-01-23 13:14:28 +0800

    Added mui tooltip

--- src/components/DateDetails/NotesView/NoteTypeTooltip/NoteTypeTooltip.tsx
+++ src/components/DateDetails/NotesView/NoteTypeTooltip/NoteTypeTooltip.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import './NoteTypeTooltip.css';
 import { NoteType } from '../../../../App';
+import { Tooltip, TooltipProps, tooltipClasses } from '@mui/material';
+import { styled } from '@mui/material/styles';
 
 const getNoteTypeName = (noteType : NoteType) => {
   switch (noteType) {
@@ -15,38 +17,37 @@ const getNoteTypeName = (noteType : NoteType) => {
   }
 }
 
+const CustomStyledTooltip = styled(({ className, ...props }: TooltipProps) => (
+  <Tooltip {...props} arrow classes={{ popper: className }} />
+))(() => ({
+  [`& .${tooltipClasses.arrow}`]: {
+    color: "rgb(30, 30, 80)"
+  },
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: "rgb(30, 30, 80)"
+  },
+}));
+
 function NoteTypeTooltip(props: any) {
-
-  const [isTooltipVisible, setTooltipVisible] = useState(false);
-
-  const handleMouseEnter = () => {
-    setTooltipVisible(true);
-  };
-
-  const handleMouseLeave = () => {
-    setTooltipVisible(false);
-  };
-
   return (
-    <div id="noteTypeTooltipParent" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-      {props.children}
-      {isTooltipVisible && (
-        <div id="noteTypeTooltip" className={props.tooltipDirection === "above" ? "noteTypeTooltipAbove" : "noteTypeTooltipRight"}>
-          <p id="noteTypeTooltipName">{getNoteTypeName(props.selectedNoteType)}</p>
-          <div id="noteTypeTooltipSelectionRow">
-            <div className={"notesTypeTooltipIconDiv notesTypeTooltipMiscDiv" + (props.selectedNoteType === NoteType.Misc ? " selectedNoteType" : "")}>
-              <img className="notesTypeTooltipIcon" src="NoteIcons/notes_misc_icon.png" onClick={() => props.onSelectionChange(NoteType.Misc)}></img>
-            </div>
-            <div className={"notesTypeTooltipIconDiv notesTypeTooltipReminderDiv" + (props.selectedNoteType === NoteType.Reminder ? " selectedNoteType" : "")}>
-              <img className="notesTypeTooltipIcon" src="NoteIcons/notes_reminder_icon.png" onClick={() => props.onSelectionChange(NoteType.Reminder)}></img>
-            </div>
-            <div className={"notesTypeTooltipIconDiv notesTypeTooltipBirthdayDiv" + (props.selectedNoteType === NoteType.Birthday ? " selectedNoteType" : "")}>
-              <img className="notesTypeTooltipIcon" src="NoteIcons/notes_birthday_icon.png" onClick={() => props.onSelectionChange(NoteType.Birthday)}></img>
-            </div>
+    <CustomStyledTooltip enterDelay={0} arrow placement="top" title={
+      <div>
+        <p id="noteTypeTooltipName">{getNoteTypeName(props.selectedNoteType)}</p>
+        <div id="noteTypeTooltipSelectionRow">
+          <div className={"notesTypeTooltipIconDiv notesTypeTooltipMiscDiv" + (props.selectedNoteType === NoteType.Misc ? " selectedNoteType" : "")}>
+            <img className="notesTypeTooltipIcon" src="NoteIcons/notes_misc_icon.png" onClick={() => props.onSelectionChange(NoteType.Misc)}></img>
+          </div>
+          <div className={"notesTypeTooltipIconDiv notesTypeTooltipReminderDiv" + (props.selectedNoteType === NoteType.Reminder ? " selectedNoteType" : "")}>
+            <img className="notesTypeTooltipIcon" src="NoteIcons/notes_reminder_icon.png" onClick={() => props.onSelectionChange(NoteType.Reminder)}></img>
+          </div>
+          <div className={"notesTypeTooltipIconDiv notesTypeTooltipBirthdayDiv" + (props.selectedNoteType === NoteType.Birthday ? " selectedNoteType" : "")}>
+            <img className="notesTypeTooltipIcon" src="NoteIcons/notes_birthday_icon.png" onClick={() => props.onSelectionChange(NoteType.Birthday)}></img>
           </div>
         </div>
-      )}
-    </div>
+      </div>
+    }>
+      {props.children}
+    </CustomStyledTooltip>
   );
 }
 
